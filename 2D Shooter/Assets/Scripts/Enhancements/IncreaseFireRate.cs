9446857bc0bd6e5a117988a681bc3042f4ecaa9f
EMRE GELEN <emregeln@gmail.com>
2022-04-03 17:35:06 +0300

    Added increase firerate script.

---
+++ 2D Shooter/Assets/Scripts/Enhancements/IncreaseFireRate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Enhancements
+{
+    public class IncreaseFireRate : MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            GameObject gameObject = collision.gameObject;
+            if (gameObject.tag == "Player")
+            {
+                foreach (Transform item in gameObject.GetComponentsInChildren<Transform>())
+                {
+                    ShootingController shootingController = item.GetComponent<ShootingController>();
+                    if (shootingController != null && shootingController.fireRate > 0.3)
+                        shootingController.fireRate -= 0.05f; 
+                }
+            }
+        }
+    }
+}
